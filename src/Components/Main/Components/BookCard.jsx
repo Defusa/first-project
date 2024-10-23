e73c173e2e69c532d871bbd494e0b2f2db6f6ebd
defusa <alexreal1999@gmail.com>
2024-10-23 18:33:12 +0300

    done

--- src/Components/Main/Components/BookCard.jsx
+++ src/Components/Main/Components/BookCard.jsx
@@ -12,7 +12,7 @@ function BookCard() {
             <h3 class="card-title">1984</h3>
             <h5 class="card-text">George Orwell</h5>
             <p class="card-text">Главный герой, Уинстон Смит, живет в тоталитарном обществе Океании, где Правительство контролирует каждую деталь жизни. Он начинает сомневаться в системе и ищет способы сопротивления, но сталкивается с жестокими последствиями.</p>
-            <h6 className="text-secondary text-end">350 страниц</h6>
+            <h6 className="text-secondary text-end">360 страниц</h6>
         </div>
         </div>
     );
